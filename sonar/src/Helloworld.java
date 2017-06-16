1d6d94787a18dcd6f72d0807489c5442d3fcc3d3
vpulyala <venugopalreddy.pulyala@gmail.com>
2017-06-16 17:35:27 +0530

    Modified
    Modified

--- sonar/src/Helloworld.java
+++ sonar/src/Helloworld.java
@@ -1,13 +1,7 @@
 public class Helloworld {
-
+int i;
   public void printHelloWorld() {
     System.out.println("Hello World!");
   }
   
 }
-
-sonar.projectKey=Helloworld
-sonar.projectName=Helloworld
-sonar.projectVersion=1.0
-
-sonar.sources=C:\Users\vpulyala.OSIUS\.jenkins\jobs\FAS_Sonar\Helloworld
