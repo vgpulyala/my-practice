30198d71dab0b61568ca139249cbef4b737c0dbc
vpulyala <venugopalreddy.pulyala@gmail.com>
2017-06-16 15:55:40 +0530

    new folder added
    new folder added

---
+++ sonar/src/Helloworld.java
@@ -0,0 +1,13 @@
+public class Helloworld {
+
+  public void printHelloWorld() {
+    System.out.println("Hello World!");
+  }
+  
+}
+
+sonar.projectKey=Helloworld
+sonar.projectName=Helloworld
+sonar.projectVersion=1.0
+
+sonar.sources=C:\Users\vpulyala.OSIUS\.jenkins\jobs\FAS_Sonar\Helloworld
